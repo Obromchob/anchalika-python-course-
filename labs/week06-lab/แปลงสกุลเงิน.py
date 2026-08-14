366dcdc0e27a33baae095bf9c40d7ead8e29a711
Obromchob <anchalika.o@ku.th>
2026-08-14 15:10:49 +0700

    ตังหมดแล้วค่าาา

---
+++ labs/week06-lab/แปลงสกุลเงิน.py
@@ -0,0 +1,25 @@
+#เขียน function ชื่อ convert_currency(value, currency)
+#ที่ทำหน้าที่ในการแปลงเงิน
+#THB <-> SUD กำหนดให้ 1 USD = 33 THB
+
+#ทั้งนี้ให้ function ดังกล่าว รับข้อมูล จำนวนเงินที่ต้องการแปลง และสกุลเงินปลายทาง
+#ตัวอย่างการเรียกใช้
+#convert_currency(100, "USD")
+#convert_currency(100, "THB")
+
+#ตัวอย่างหน้าจอ
+#100 THB = 3.33 USD
+#100 USD = 3300.0 THB
+
+def convert_currency(value, currency):
+    result = 0
+    if currency == "USD":
+        result = value / 33.0
+        print(f"{value} THB = {result} USD")
+    else:
+        result = value / 33.0
+        print(f"{value} USD = {result} THB")
+              
+convert_currency(100, "USD")
+convert_currency(100, "THB")
+
