d510daec0d45ad0890fb626eeaca2a4c0c0d1536
Obromchob <anchalika.o@ku.th>
2026-09-18 14:44:20 +0700

    18/9/2569

---
+++ labs/week09-lab-exception/test.py
@@ -0,0 +1,45 @@
+"""
+
+โจทย์ 1: เครื่องคำนวณอย่างปลอดภัย
+เขียนโปรแกรมรับตัวเลข 2 จำนวนและดำเนินการ 1 ตัว ได้แก่ + - * / แล้วแสดงผลลัพธ์
+โปรแกรมต้องจัดการกรณีต่อไปนี้
+    ผู้ใช้กรอกข้อมูลที่ไม่ใช่ตัวเลข#ValueError
+    ผู้ใช้เลือกตัวดำเนินการอื่นนอกเหนือจาก + - * / raise ValueError
+    ผู้ใช้พยายามหารศูนย์ด้วย #ZeroDivisionError
+    โปรแกรมต้องแสดง จบการทำงาน เสมอด้วย finally
+
+ตัวอย่างผลลัพธ์ที่คาดหวัง
+
+ตัวเลขที่ 1: 10
+ตัวเลขที่ 2: 0
+เครื่องหมาย = (+, -, *, /) /
+
+ไม่สามารถหารด้วยศูนย์ได้
+จบการทำงาน
+
+"""
+
+try:
+    num1 = float(input("ตัวเลขที่1: "))
+    num2 = float(input("ตัวเลขที่2: "))
+    operator = float(input("เครื่องหมาย (+, -, *, /): "))
+
+    result = 0
+    if operator == "+":
+        result = num1 + num2
+    elif operator == "-":
+        result = num1 - num2    
+    elif operator == "*":
+        result = num1 * num2
+    elif operator == "/":
+        result = num1 / num2
+    else:
+        raise ValueError("เครื่องหมายต้องเป็น + - * / เท่านั้น")
+
+    print(f"{num1} {operator} {num2} {result}")
+except ValueError:
+    print("กรุณากรอกตัวเลขเท่านั้น")
+except ZeroDivisionError:
+    print("ไม่สามารถหารด้วยศูนย์ได้")
+finally:
+    print("จบการตรวจสอบคะแนน")
